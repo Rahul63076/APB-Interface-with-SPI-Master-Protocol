a14c80cde552f9081d4a239212c0b99e53eef8c9
Rahul Shukla <114052202+Rahul63076@users.noreply.github.com>
2025-11-22 21:38:02 +0530

    Update module instantiations with named parameters

--- TOP_SPI_Block.v
+++ TOP_SPI_Block.v
@@ -14,37 +14,37 @@ module SPI_Top_Block(
   // Instantiate the Baud_Rate_Generator RTL module with named base
   
 	Baud_Rate_Generator module_1(
-		.PCLK(PCLK), 
-		.PRESET_n(PRESET_n), 
-		.spiswai_i(spiswai_i), 
-		.cpol_i(cpol_i), 
-		.cpha_i(cpha_i), 
-		.ss_i(ss_i), 
-		.sppr_i(sppr_i), 
-		.spr_i(spr_i), 
-		.spi_mode_i(spi_mode), 
-		.miso_receive_sclk_o(miso_receive_sclk_o), 
-		.miso_receive_sclk0_o(miso_receive_sclk0_o), 
-		.mosi_send_sclk_o(mosi_send_sclk_o), 
-		.mosi_send_sclk0_o(mosi_send_sclk0_o), 
-		.sclk_o(sclk_o), 
-		.BaudRateDivisor_o(baudratedivisor)
+		 PCLK, 
+		 PRESET_n, 
+		 spiswai, 
+		 cpol, 
+		 cpha, 
+		 ss, 
+		 sppr, 
+		 spr, 
+		 spi_mode, 
+		 miso_receive_sclk, 
+		 miso_receive_sclk0, 
+		 mosi_send_sclk, 
+		 mosi_send_sclk0, 
+		 sclk, 
+		 BaudRateDivisor
 	);
 
 
   // Instantiate the Slave_Select_Generator RTL module with named base
   
 	Slave_Select_Generator module_2(
-		.PCLK(PCLK), 
-		.PRESET_n(PRESET_n), 
-		.mstr_i(mstr_i), 
-		.send_data_i(send_data_i), 
-		.spiswa_i(spiswa_i), 
-		.spi_mode_i(spi_mode_i), 
-		.BaudRateDivisor_i(baudratedivisor), 
-		.ss_o(ss_o), 
-		.receive_data_o(receive_data_o), 
-		.tip_o(tip_o)
+		 PCLK, 
+		 PRESET_n, 
+		 mstr, 
+		 send_data, 
+		 spiswai, 
+		 spi_mode, 
+		 BaudRateDivisor, 
+		 ss, 
+		 receive_data, 
+		 tip
 	);
 
   
@@ -52,53 +52,53 @@ module SPI_Top_Block(
 
   
 	SPI_Shifter module_3(
-		.PCLK(PCLK), 
-		.PRESET_n(PRESET_n), 
-		.ss_i(ss_i), 
-		.send_data_i(send_data_i), 
-		.lsbfe_i(lsbfe_i), 
-		.cpol_i(cpol_i), 
-		.cpha_i(cpha_i), 
-		.receive_data_i(receive_data_i), 
-		.miso_i(miso_i), 
-		.miso_receive_sclk_o(miso_receive_sclk_o), 
-		.mosi_send_sclk_o(mosi_send_sclk_o), 
-		.miso_receive_sclk0_o(miso_receive_sclk0_o), 
-		.mosi_send_sclk0_o(mosi_send_sclk0_o), 
-		.data_mosi_i(data_mosi), 
-		.mosi_o(mosi_o), 
-		.data_miso_o(data_miso_o)
+		 PCLK, 
+		 PRESET_n, 
+		 ss, 
+		 send_data, 
+		 lsbfe, 
+		 cpol, 
+		 cpha, 
+		 receive_data, 
+		 miso, 
+		 miso_receive_sclk, 
+		 mosi_send_sclk, 
+		 miso_receive_sclk0, 
+		 mosi_send_sclk0, 
+		 data_mosi, 
+		 mosi, 
+		 data_miso
 	);
 
 
   // Instantiate the APB_Slave_Interface RTL module with named base
   
 	APB_Slave_Interface module_4(
-		.PCLK(PCLK), 
-		.PRESET_n(PRESET_n), 
-		.PWRITE_i(PWRITE_i), 
-		.PSEL_i(PSEL_i), 
-		.PENABLE_i(PENABLE_i), 
-		.ss_i(ss_i), 
-		.receive_data_i(receive_data_i), 
-		.tip_i(tip_i), 
-		.PADDR_i(PADDR_i), 
-		.PWDATA_i(PWDATA_i), 
-		.miso_data_i(miso_data_i), 
-		.mstr_o(mstr_o), 
-		.cpol_o(cpol_o), 
-		.cpha_o(cpha_o), 
-		.lsbfe_o(lsbfe_o), 
-		.spiswai_o(spiswai_o), 
-		.PREADY_o(PREADY_o), 
-		.PSLVERR_o(PSLVERR_o), 
-		.spi_mode_o(spi_mode), 
-		.send_data_o(send_data_o), 
-		.spi_interrupt_request_o(spi_interrupt_request_o), 
-		.mosi_data_o(mosi_data), 
-		.sppr_o(sppr), 
-		.spr_o(spr), 
-		.PRDATA_o(PRDATA_o)
+		 PCLK, 
+		 PRESET_n, 
+		 PWRITE, 
+		 PSEL, 
+		 PENABLE, 
+		 ss, 
+		 receive_data, 
+		 tip, 
+		 PADDR, 
+		 PWDATA, 
+		 data_miso, 
+		 mstr, 
+		 cpol, 
+		 cpha, 
+		 lsbfe, 
+		 spiswai, 
+		 PREADY, 
+		 PSLVERR, 
+		 spi_mode, 
+		 send_data, 
+		 spi_interrupt_request, 
+		 data_mosi, 
+		 sppr, 
+		 spr, 
+		 PRDATA
 	);
 
 endmodule
